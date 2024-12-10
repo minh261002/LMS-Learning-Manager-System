b95468ab85183d0130753024f4e2a7b12f3d3588
minhttran <trcongminh.261002@gmail.com>
2024-12-10 14:21:43 +0700

    Update logo paths in admin layout and add category item partial for frontend

--- resources/views/frontend/partials/header.blade.php
+++ resources/views/frontend/partials/header.blade.php
@@ -1,3 +1,7 @@
+@php
+    $categories = \App\Models\Category::get()->toTree();
+@endphp
+
 <header class="header-menu-area bg-white">
     <div class="header-top pr-150px pl-150px border-bottom border-bottom-gray py-1">
         <div class="container-fluid">
@@ -40,9 +44,9 @@
                         <ul
                             class="generic-list-item d-flex flex-wrap align-items-center fs-14 border-left border-left-gray pl-3 ml-3">
                             <li class="d-flex align-items-center pr-3 mr-3 border-right border-right-gray"><i
-                                    class="la la-sign-in mr-1"></i><a href="login.html"> Login</a></li>
+                                    class="la la-sign-in mr-1"></i><a href="login.html"> Đăng nhập</a></li>
                             <li class="d-flex align-items-center"><i class="la la-user mr-1"></i><a href="sign-up.html">
-                                    Register</a></li>
+                                    Đăng ký</a></li>
                         </ul>
                     </div><!-- end header-widget -->
                 </div><!-- end col-lg-6 -->
@@ -56,7 +60,8 @@
                 <div class="row align-items-center">
                     <div class="col-lg-2">
                         <div class="logo-box">
-                            <a href="index.html" class="logo"><img src="images/logo.png" alt="logo"></a>
+                            <a href="index.html" class="logo"><img src="{{ asset('frontend/images/logo.png') }}"
+                                    alt="logo"></a>
                             <div class="user-btn-action">
                                 <div class="search-menu-toggle icon-element icon-element-sm shadow-sm mr-2"
                                     data-toggle="tooltip" data-placement="top" title="Search">
@@ -78,137 +83,30 @@
                             <div class="menu-category">
                                 <ul>
                                     <li>
-                                        <a href="#">Categories <i class="la la-angle-down fs-12"></i></a>
+                                        <a href="#">Danh mục <i class="la la-angle-down fs-12"></i></a>
                                         <ul class="cat-dropdown-menu">
-                                            <li>
-                                                <a href="course-grid.html">Development <i
-                                                        class="la la-angle-right"></i></a>
-                                                <ul class="sub-menu">
-                                                    <li><a href="#">All Development</a></li>
-                                                    <li><a href="#">Web Development</a></li>
-                                                    <li><a href="#">Mobile Apps</a></li>
-                                                    <li><a href="#">Game Development</a></li>
-                                                    <li><a href="#">Databases</a></li>
-                                                    <li><a href="#">Programming Languages</a></li>
-                                                    <li><a href="#">Software Testing</a></li>
-                                                    <li><a href="#">Software Engineering</a></li>
-                                                    <li><a href="#">E-Commerce</a></li>
-                                                </ul>
-                                            </li>
-                                            <li>
-                                                <a href="course-grid.html">business <i
-                                                        class="la la-angle-right"></i></a>
-                                                <ul class="sub-menu">
-                                                    <li><a href="#">All Business</a></li>
-                                                    <li><a href="#">Finance</a></li>
-                                                    <li><a href="#">Entrepreneurship</a></li>
-                                                    <li><a href="#">Strategy</a></li>
-                                                    <li><a href="#">Real Estate</a></li>
-                                                    <li><a href="#">Home Business</a></li>
-                                                    <li><a href="#">Communications</a></li>
-                                                    <li><a href="#">Industry</a></li>
-                                                    <li><a href="#">Other</a></li>
-                                                </ul>
-                                            </li>
-                                            <li>
-                                                <a href="course-grid.html">IT & Software <i
-                                                        class="la la-angle-right"></i></a>
-                                                <ul class="sub-menu">
-                                                    <li><a href="#">All IT & Software</a></li>
-                                                    <li><a href="#">IT Certification</a></li>
-                                                    <li><a href="#">Hardware</a></li>
-                                                    <li><a href="#">Network & Security</a></li>
-                                                    <li><a href="#">Operating Systems</a></li>
-                                                    <li><a href="#">Other</a></li>
-                                                </ul>
-                                            </li>
-                                            <li>
-                                                <a href="course-grid.html">Finance & Accounting <i
-                                                        class="la la-angle-right"></i></a>
-                                                <ul class="sub-menu">
-                                                    <li><a href="#"> All Finance & Accounting</a></li>
-                                                    <li><a href="#">Accounting & Bookkeeping</a></li>
-                                                    <li><a href="#">Cryptocurrency & Blockchain</a></li>
-                                                    <li><a href="#">Economics</a></li>
-                                                    <li><a href="#">Investing & Trading</a></li>
-                                                    <li><a href="#">Other Finance & Economics</a></li>
-                                                </ul>
-                                            </li>
-                                            <li>
-                                                <a href="course-grid.html">design <i
-                                                        class="la la-angle-right"></i></a>
-                                                <ul class="sub-menu">
-                                                    <li><a href="#">All Design</a></li>
-                                                    <li><a href="#">Graphic Design</a></li>
-                                                    <li><a href="#">Web Design</a></li>
-                                                    <li><a href="#">Design Tools</a></li>
-                                                    <li><a href="#">3D & Animation</a></li>
-                                                    <li><a href="#">User Experience</a></li>
-                                                    <li><a href="#">Other</a></li>
-                                                </ul>
-                                            </li>
-                                            <li>
-                                                <a href="course-grid.html">Personal Development <i
-                                                        class="la la-angle-right"></i></a>
-                                                <ul class="sub-menu">
-                                                    <li><a href="#">All Personal Development</a></li>
-                                                    <li><a href="#">Personal Transformation</a></li>
-                                                    <li><a href="#">Productivity</a></li>
-                                                    <li><a href="#">Leadership</a></li>
-                                                    <li><a href="#">Personal Finance</a></li>
-                                                    <li><a href="#">Career Development</a></li>
-                                                    <li><a href="#">Parenting & Relationships</a></li>
-                                                    <li><a href="#">Happiness</a></li>
-                                                </ul>
-                                            </li>
-                                            <li>
-                                                <a href="course-grid.html">Marketing <i
-                                                        class="la la-angle-right"></i></a>
-                                                <ul class="sub-menu">
-                                                    <li><a href="#">All Marketing</a></li>
-                                                    <li><a href="#">Digital Marketing</a></li>
-                                                    <li><a href="#">Search Engine Optimization</a></li>
-                                                    <li><a href="#">Social Media Marketing</a></li>
-                                                    <li><a href="#">Branding</a></li>
-                                                    <li><a href="#">Video & Mobile Marketing</a></li>
-                                                    <li><a href="#">Affiliate Marketing</a></li>
-                                                    <li><a href="#">Growth Hacking</a></li>
-                                                    <li><a href="#">Other</a></li>
-                                                </ul>
-                                            </li>
-                                            <li>
-                                                <a href="course-grid.html">Health & Fitness <i
-                                                        class="la la-angle-right"></i></a>
-                                                <ul class="sub-menu">
-                                                    <li><a href="#">All Health & Fitness</a></li>
-                                                    <li><a href="#">Fitness</a></li>
-                                                    <li><a href="#">Sports</a></li>
-                                                    <li><a href="#">Dieting</a></li>
-                                                    <li><a href="#">Self Defense</a></li>
-                                                    <li><a href="#">Meditation</a></li>
-                                                    <li><a href="#">Mental Health</a></li>
-                                                    <li><a href="#">Yoga</a></li>
-                                                    <li><a href="#">Dance</a></li>
-                                                    <li><a href="#">Other</a></li>
-                                                </ul>
-                                            </li>
-                                            <li>
-                                                <a href="course-grid.html">Photography <i
-                                                        class="la la-angle-right"></i></a>
-                                                <ul class="sub-menu">
-                                                    <li><a href="#">All Photography</a></li>
-                                                    <li><a href="#">Digital Photography</a></li>
-                                                    <li><a href="#">Photography Fundamentals</a></li>
-                                                    <li><a href="#">Commercial Photography</a></li>
-                                                    <li><a href="#">Video Design</a></li>
-                                                    <li><a href="#">Photography Tools</a></li>
-                                                    <li><a href="#">Other</a></li>
-                                                </ul>
-                                            </li>
+                                            @foreach ($categories as $category)
+                                                <li>
+                                                    <a href="#">{{ $category->name }}
+                                                        @if ($category->children->isNotEmpty())
+                                                            <i class="la la-angle-right"></i>
+                                                        @endif
+                                                    </a>
+                                                    @if ($category->children->isNotEmpty())
+                                                        <ul class="sub-menu">
+                                                            @include('frontend.partials.category-item', [
+                                                                'categories' => $category->children,
+                                                            ])
+                                                        </ul>
+                                                    @endif
+                                                </li>
+                                            @endforeach
                                         </ul>
+
                                     </li>
                                 </ul>
-                            </div><!-- end menu-category -->
+                            </div>
+
                             <form method="post">
                                 <div class="form-group mb-0">
                                     <input class="form-control form--control pl-3" type="text" name="search"
@@ -216,6 +114,7 @@
                                     <span class="la la-search search-icon"></span>
                                 </div>
                             </form>
+
                             <nav class="main-menu">
                                 <ul>
                                     <li>
@@ -322,7 +221,8 @@
                                         </ul>
                                     </li>
                                 </ul><!-- end ul -->
-                            </nav><!-- end main-menu -->
+                            </nav>
+
                             <div class="shop-cart mr-4">
                                 <ul>
                                     <li>
@@ -369,7 +269,8 @@
                                         </ul>
                                     </li>
                                 </ul>
-                            </div><!-- end shop-cart -->
+                            </div>
+
                             <div class="nav-right-button">
                                 <a href="admission.html" class="btn theme-btn d-none d-lg-inline-block"><i
                                         class="la la-user-plus mr-1"></i> Admission</a>
